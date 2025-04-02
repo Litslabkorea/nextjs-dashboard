d80abf9af31f662a267e1ef69cfcd5bdbfdc140e
Litslabkorea <bumyoub.lee@gmail.com>
2025-04-03 00:42:56 +0900

    대시보드 페이지에서 수익 데이터를 콘솔에 출력하도록 수정

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -8,7 +8,7 @@ export default async function Page() {
   const revenue = await fetchRevenue();
   const latestInvoices = await fetchLatestInvoices();
   const { numberOfInvoices, numberOfCustomers, totalPaidInvoices, totalPendingInvoices } = await fetchCardData();
-  
+  console.log(revenue);
   return (
     <main>
       <h1 className={`${lusitana.className} mb-4 text-xl md:text-2xl`}>
