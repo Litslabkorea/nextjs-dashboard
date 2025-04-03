5162121131fdd68e959fa62acaf41579b3096e6a
Litslabkorea <bumyoub.lee@gmail.com>
2025-04-03 23:47:27 +0900

    인증 및 인보이스 관리 기능 추가, 로그인 페이지 및 오류 처리 UI 컴포넌트 구현. ESLint 설정 추가 및 관련 의존성 업데이트.

--- app/ui/dashboard/sidenav.tsx
+++ app/ui/dashboard/sidenav.tsx
@@ -2,6 +2,7 @@ import Link from 'next/link';
 import NavLinks from '@/app/ui/dashboard/nav-links';
 import AcmeLogo from '@/app/ui/acme-logo';
 import { PowerIcon } from '@heroicons/react/24/outline';
+import { signOut } from '@/auth';
 
 // SideNav 컴포넌트는 대시보드의 사이드바를 구현합니다:
 // 1. 상단에 Acme 로고와 홈 링크
@@ -23,7 +24,12 @@ export function SideNav() {
       <div className="flex grow flex-row justify-between space-x-2 md:flex-col md:space-x-0 md:space-y-2">
         <NavLinks />
         <div className="hidden h-auto w-full grow rounded-md bg-gray-50 md:block"></div>
-        <form>
+        <form 
+          action={async () => {
+          'use server';
+          await signOut({ redirectTo: '/login' });
+          }}
+        >
           <button className="flex h-[48px] w-full grow items-center justify-center gap-2 rounded-md bg-gray-50 p-3 text-sm font-medium hover:bg-sky-100 hover:text-blue-600 md:flex-none md:justify-start md:p-2 md:px-3">
             <PowerIcon className="w-6" />
             <div className="hidden md:block">Sign Out</div>
